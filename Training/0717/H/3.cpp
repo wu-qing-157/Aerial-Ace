4d869967d24f79e7e6da1a6807f39cf825f5c05f
wu-qing-157 <wuqing157@gmail.com>
2019-07-17 19:26:33 +0800

    training 0717 no-use try

--- Training/0717/H/3.cpp
+++ Training/0717/H/3.cpp
@@ -5,7 +5,7 @@
 #include <unordered_set>
 #include <vector>
 
-const double eps = 1e-9, pi = std::acos(-1);
+const double eps = 1e-7, pi = std::acos(-1);
 
 int sgn(double x) {
     return (x > eps) - (x < -eps);
@@ -22,9 +22,6 @@ struct point {
     friend point operator-(const point &a, const point &b) {
         return point(a.x - b.x, a.y - b.y);
     }
-    point operator-() const {
-        return point(-x, -y);
-    }
     friend point operator*(const point &a, T &b) {
         return point(a.x * b, a.y * b);
     }
@@ -36,6 +33,9 @@ struct point {
     }
     T len2() const {
         return x * x + y * y;
+    }
+    double len() const {
+        return std::sqrt(len2());
     }
     double angle() const {
         return std::atan2(y, x);
@@ -94,7 +94,7 @@ bool cmp2(const point<int64_t> &a, const point<int64_t> &b) {
 
 bool merge(line<int64_t> &a, const line<int64_t> &b) {
     if (det(a.v(), b.v()) != 0) return false;
-    if (det(a.v(), b.b - a.a) != 0) return false;
+    if (det(a.v(), b.b - a.a)) return false;
     if ((a.a.y == a.b.y && a.b.x >= b.a.x) || a.b.y >= b.a.y) {
         a = line<int64_t>(std::min(a.a, b.a, cmp2), std::max(a.b, b.b, cmp2));
         return true;
@@ -119,11 +119,11 @@ void process(std::vector<line<int64_t>> &ret) {
         }
         cur = cur + point<int64_t>(dx, dy);
     }
-    puts("READ");
-    for (auto i : sa) printf("(%ld, %ld) -> (%ld, %ld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
+    //puts("READ");
+    //for (auto i : sa) printf("(%ld, %ld) -> (%ld, %ld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
     std::sort(sa.begin(), sa.end(), cmp);
-    puts("SORTED");
-    for (auto i : sa) printf("(%ld, %ld) -> (%ld, %ld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
+    //puts("SORTED");
+    //for (auto i : sa) printf("(%ld, %ld) -> (%ld, %ld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
     for (int i = 0; i < (int) sa.size(); i++) {
         line<int64_t> cur = sa[i];
         while (i + 1 < (int) sa.size() && merge(cur, sa[i + 1])) i++;
@@ -131,30 +131,30 @@ void process(std::vector<line<int64_t>> &ret) {
     }
 }
 
-bool trans(const point<int64_t> a, double rotate, double scale2, point<int64_t> &ret) {
-    double len2 = a.len2() * scale2;
+bool trans(const point<int64_t> a, double rotate, double scale, point<int64_t> &ret) {
+    double len = a.len() * scale;
     double angle = a.angle() + rotate;
-    double x = std::sqrt(len2) * std::cos(angle);
-    double y = std::sqrt(len2) * std::sin(angle);
-    ret.x = std::llround(x);
-    ret.y = std::llround(y);
-    // printf("%f %f (%ld, %ld) --> (%f, %f)(%ld, %ld)\n", rotate, scale, a.x, a.y, x, y, ret.x, ret.y);
+    double x = len * std::cos(angle);
+    double y = len * std::sin(angle);
+    ret.x = std::round(x);
+    ret.y = std::round(y);
+    //printf("%f %f (%ld, %ld) --> (%f, %f)(%ld, %ld)\n", rotate, scale, a.x, a.y, x, y, ret.x, ret.y);
     return sgn(ret.x - x) == 0 && sgn(ret.y - y) == 0;
 }
 
-bool judge(const point<int64_t> &delta1, const point<int64_t> &delta2, double rotate, double scale2) {
-    // printf("case (%ld, %ld) %ld %ld\n", delta.x, delta.y, rotate, scale);
+bool judge(const point<int64_t> delta, double rotate, double scale) {
+    //printf("case (%ld, %ld) %ld %ld\n", delta.x, delta.y, rotate, scale);
     set.clear();
     for (auto i : b) set.insert(i);
     for (auto i : a) {
         line<int64_t> l;
-        if (!trans(i.a + delta1, rotate, scale2, l.a) ||
-                !trans(i.b + delta1, rotate, scale2, l.b))
+        if (!trans(i.a, rotate, scale, l.a) ||
+                !trans(i.b, rotate, scale, l.b))
             return false;
-        l.a = l.a + delta2;
-        l.b = l.b + delta2;
+        l.a = l.a + delta;
+        l.b = l.b + delta;
         pure_line(l);
-        printf("(%ld, %ld) -> (%ld %ld)\n", l.a.x, l.a.y, l.b.x, l.b.y);
+        //printf("(%ld, %ld) -> (%ld %ld)\n", l.a.x, l.a.y, l.b.x, l.b.y);
         if (!set.count(l))
             return false;
     }
@@ -165,10 +165,10 @@ int main() {
     while (true) {
         process(a);
         process(b);
-        puts("A");
-        for (auto i : a) printf("(%lld, %lld) -> (%lld, %lld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
-        puts("B");
-        for (auto i : b) printf("(%lld, %lld) -> (%lld, %lld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
+        //puts("A");
+        //for (auto i : a) printf("(%lld, %lld) -> (%lld, %lld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
+        //puts("B");
+        //for (auto i : b) printf("(%lld, %lld) -> (%lld, %lld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
         if (a.size() != b.size()) {
             puts("NO");
             continue;
@@ -179,11 +179,11 @@ int main() {
         }
         bool flag = true;
         for (auto i : b) {
-            printf("try (%lld, %lld) -> (%lld, %lld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
-            if (judge(-a[0].a, i.a, i.v().angle() - a[0].v().angle(),
-                        ((double) i.v().len2()) / a[0].v().len2()) ||
-                    judge(-a[0].a, i.b, i.v().angle() - a[0].v().angle() + pi,
-                        ((double) i.v().len2()) / a[0].v().len2())) {
+            //printf("try (%lld, %lld) -> (%lld, %lld)\n", i.a.x, i.a.y, i.b.x, i.b.y);
+            if (judge(i.a - a[0].a, i.v().angle() - a[0].v().angle(),
+                        i.v().len() / a[0].v().len()) ||
+                    judge(i.b - a[0].a, i.v().angle() - a[0].v().angle() + pi,
+                        i.v().len() / a[0].v().len())) {
                 puts("YES");
                 flag = false;
                 break;
